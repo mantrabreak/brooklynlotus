527e1a9fcaa4e6fa7d7eff431aba489f3184cb5c
Sid Nutulapati <sid.nutulapati@nytimes.com>
2020-10-09 15:57:31 -0400

    ipad done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Media from 'react-media';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import './large.css';
-// import './medium.css';
+import './medium.css';
 // import './small.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,22 +15,52 @@ import Icons from './Icons.js'
 
 function App() {
   return (
-    // Large
-    <Container fluid className="full">
-      <Row className="lotus-row">
-        <Lotus/>
-      </Row>
-      <Row className="footer">
-        <Col xl={4} lg={4} className="logo">
-          <Logo/>
-        </Col>
-        <Col xl={4} lg={4} className="empty">
-        </Col>
-        <Col xl={4} lg={4} className="icons">
-          <Icons/>
-        </Col>
-      </Row>
-    </Container>
+    <Media queries={{
+      small: "(max-width: 699px)",
+      medium: "(min-width: 700px) and (max-width: 1149px)",
+      large: "(min-width: 1150px)"
+    }}>
+      {matches => (
+        <div>
+          {
+            matches.large &&
+            // Large
+            <Container fluid className="full">
+              <Row className="lotus-row">
+                <Lotus />
+              </Row>
+              <Row className="footer">
+                <Col xl={4} lg={4} md={4} className="logo">
+                  <Logo />
+                </Col>
+                <Col xl={4} lg={4} md={2} className="empty">
+                </Col>
+                <Col xl={4} lg={4} md={4} className="icons">
+                  <Icons />
+                </Col>
+              </Row>
+            </Container>
+          }
+          {
+            matches.medium &&
+            // Medium
+            <Container fluid className="full">
+              <Row className="lotus-row">
+                <Lotus />
+              </Row>
+              <Row className="footer">
+                <Col md={6} lg={6} className="logo">
+                  <Logo />
+                </Col>
+                <Col md={6} lg={6} className="icons">
+                  <Icons />
+                </Col>
+              </Row>
+            </Container>
+          }
+          </div>
+      )}
+      </Media>
   );
 }
 
