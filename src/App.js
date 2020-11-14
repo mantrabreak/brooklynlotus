172929281e801521a3acb2ae24e9002329971598
Sid Nutulapati <sid.nutulapati@nytimes.com>
2020-11-14 12:56:52 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -15,20 +15,20 @@ import Icons from './Icons.js'
 
 function App() {
   return (
-    <Media queries={{
-      small: "(max-width: 699px)",
-      medium: "(min-width: 700px) and (max-width: 1149px)",
-      large: "(min-width: 1150px)"
-    }}>
-      {matches => (
-        <div>
-          {
-            matches.large &&
-            // Large
-            <Container fluid className="full">
-              <Row className="lotus-row">
-                <Lotus />
-              </Row>
+    <Container fluid className="full">
+      <Row className="lotus-row">
+        <Lotus />
+      </Row>
+      <Media queries={{
+        small: "(max-width: 699px)",
+        medium: "(min-width: 700px) and (max-width: 1149px)",
+        large: "(min-width: 1150px)"
+      }}>
+        {matches => (
+          <div>
+            {
+              matches.large &&
+              // Large
               <Row className="footer">
                 <Col xl={4} lg={4} md={4} className="logo">
                   <Logo />
@@ -39,15 +39,10 @@ function App() {
                   <Icons />
                 </Col>
               </Row>
-            </Container>
-          }
-          {
-            matches.medium &&
-            // Medium
-            <Container fluid className="full">
-              <Row className="lotus-row">
-                <Lotus />
-              </Row>
+            }
+            {
+              matches.medium &&
+              // Medium
               <Row className="footer">
                 <Col md={6} lg={6} className="logo">
                   <Logo />
@@ -56,30 +51,27 @@ function App() {
                   <Icons />
                 </Col>
               </Row>
-            </Container>
-          }
-          {
-            matches.small &&
-            // Small
-            <Container fluid className="full">
-              <Row className="lotus-row">
-                <Lotus />
-              </Row>
-              <Row className="footer-icons">
-                <Col sm={12} xs={12} className="icons">
-                  <Icons />
-                </Col>
-              </Row>
-              <Row className="footer-logo">
-                <Col sm={12} xs={12} className="logo">
-                  <Logo />
-                </Col>
-              </Row>
-            </Container>
-          }
+            }
+            {
+              matches.small &&
+              // Small
+              <div>
+                <Row className="footer-icons">
+                  <Col sm={12} xs={12} className="icons">
+                    <Icons />
+                  </Col>
+                </Row>
+                <Row className="footer-logo">
+                  <Col sm={12} xs={12} className="logo">
+                    <Logo />
+                  </Col>
+                </Row>
+              </div>
+            }
           </div>
-      )}
+        )}
       </Media>
+    </Container>
   );
 }
 
